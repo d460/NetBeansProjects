9cff628f283ad2b4023f3122aad1b998fbc5c0cd
Dagoberto <Dagoberto@HP-Dago>
2016-10-23 22:34:12 -0500

    Maven example

---
+++ maven-demo/src/main/java/com/d460/maven/demo/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.d460.maven.demo;
+
+/**
+ *
+ * @author Dagoberto
+ */
+public class App {
+    
+    
+    
+}

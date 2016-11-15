83a35e47dd49d93ad6c3ac0cd1a164bb9eaaf2ca
d460 <dagovf@comunidad.unam.mx>
2016-11-14 23:32:42 -0600

    Prueba conexion DB

---
+++ PruebaConexion/src/pruebaconexion/PruebaConexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaconexion;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Dagoberto
+ */
+public class PruebaConexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFConexion jfc = new JFConexion();
+        jfc.setVisible(true);
+        jfc.setLocationRelativeTo(null);
+        
+    }
+    
+}

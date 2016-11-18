776c7daa965fc324e5fa16130d83bf0a74d637f7
d460 <dagovf@comunidad.unam.mx>
2016-11-18 12:15:14 -0600

    First WB final funcionando WS deployed

--- WSUsuario/src/java/webservices/WSGestionUsuario.java
+++ WSUsuario/src/java/webservices/WSGestionUsuario.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package webservices;
 
 import javax.jws.WebService;
@@ -10,10 +6,7 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import modelo.*;
 
-/**
- *
- * @author Dagoberto
- */
+
 @WebService(serviceName = "WSGestionUsuario")
 public class WSGestionUsuario {
 
